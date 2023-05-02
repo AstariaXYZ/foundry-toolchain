48989152b8165d7163c44e8c80e9921049569a45
Andrew Redden <opensource@andrewredden.com>
2023-05-02 14:15:55 -0300

    rollback to april 1 version

--- src/utils.js
+++ src/utils.js
@@ -17,7 +17,7 @@ function getDownloadObject(version) {
   const platform = os.platform();
   const filename = `foundry_${normalizeVersionName(version)}_${platform}_${mapArch(os.arch())}`;
   const extension = platform === "win32" ? "zip" : "tar.gz";
-  const url = `https://github.com/foundry-rs/foundry/releases/download/${version}/${filename}.${extension}`;
+  const url = `https://github.com/foundry-rs/foundry/releases/download/nightly-87bc53fc6c874bd4c92d97ed180b949e3a36d78c/${filename}.${extension}`;
 
   return {
     url,
